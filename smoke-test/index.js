7505a639ddfd4797f0b434dc32f064b767d17ccd
Rafael Echeverria <master.152@gmail.com>
2020-09-18 10:50:24 -0500

    Improve smoke test logging

--- smoke-test/index.js
+++ smoke-test/index.js
@@ -7,13 +7,32 @@ const async = require('async');
 const path = require('path');
 var os = require('os');
 var flatten = require('flat');
+const minimist = require('minimist');
+const ora = require('ora');
+const logSymbols = require('log-symbols');
 
-var totalFiles = 0, totalSuccess = 0, totalError = 0;
+const args = minimist(process.argv.slice(2))
+const help = `
+   USAGE: 
+   node index.js <options>
 
+   --source, -s ..... Images source directory
+   --output, -o ..... Output file (optional, default is [UTC Date in ms].json)
+   --port, -p   ..... The port of the local hosted instance to run against (optional, default = 8080)
+`
+
+if (args.s === undefined) {
+   console.log(help);
+   process.exit(0);
+}
+const sourceFolder = args.source || args.s;
+const outputFile = (args.output === undefined && args.o === undefined) ?`${Date.now().toString()}.json` : args.output || args.o;
+const port = (args.port === undefined && args.p === undefined) ? 8080 : args.port || args.p;
+let totalFiles = 0, totalSuccess = 0, totalError = 0, processed = 0;
 
 (async function main() {
 
-   const ws = fs.createWriteStream('./carSelfies.json');
+   const ws = fs.createWriteStream(`./${outputFile}`);
    ws.on('error', function (err) {
       console.error(err);
    });
@@ -21,16 +40,25 @@ var totalFiles = 0, totalSuccess = 0, totalError = 0;
    try {
       var i = 0;
       const tasks = [];
-      const files = await getFiles('./images/Car Selfie');
+      const files = await getFiles(sourceFolder);
       totalFiles = files.length;
-      files.forEach((f) => tasks.push(async () => processImage(ws, f)));
+      const spinner = ora({text: `Processing ${totalFiles} images`, spinner: 'star'});
+      spinner.render();
+      files.forEach((f) => tasks.push(async () => processImage(ws, f, spinner)));
+      //spinner.stop();
       await async.parallelLimit(tasks, 1);
    }
-   catch(err) {
+   catch (err) {
       console.error(err);
    }
    finally {
-      ws.close( );
+      ws.close();
+      console.log(`
+:: RESULTS ::
+ - ${logSymbols.info} Total images processed: ${totalFiles}
+ - ${logSymbols.success} Total success: ${totalSuccess}
+ - ${logSymbols.error} Total errors: ${totalError}
+      `);
    }
 })();
 
@@ -50,36 +78,36 @@ async function getFiles(dir) {
 }
 
 
-async function processImage(ws,filePath) {
-   console.log( `Processing ${filePath}` );
+async function processImage(ws, filePath, spinner) {   
    try {
-      const endpoint = "http://localhost:41960/analyzeMedia";
+      const endpoint = `http://localhost:${port}/analyzeMedia`;
       const form = new FormData();
       form.append('media', fs.createReadStream(filePath));
-      form.append('originMediaID',filePath);
-      form.append('timeOut',25000);
-      form.append('modelsToCall','{"imageMeta": 1,"imageSceneOut": 1,"imageObjects": 1,"imageTox": 1,"imagePose": 1,"faces": 1,"photoManipulation": 1}');
-      //form.append()
-
+      form.append('originMediaID', filePath);
+      form.append('timeOut', 25000);
+      form.append('modelsToCall', '{"imageMeta": 1,"imageSceneOut": 1,"imageObjects": 1,"imageTox": 1,"imagePose": 1,"faces": 1,"photoManipulation": 1}');     
+      spinner.start(`Processing ${filePath}`);
 
       const response = await axios.post(endpoint, form, { headers: form.getHeaders() });
-      await trackResult(ws, filePath, response);
       totalSuccess++;
-      console.log('     Success')
+      processed = totalError + totalSuccess;     
+      spinner.succeed(`${filePath} processed successfully, ${processed}/${totalFiles}`);
+      await trackResult(ws, filePath, response);
       return response;
    }
-   catch(e) {
+   catch (e) {
+      spinner.fail(`${filePath} processing failed, ${processed}/${totalFiles}`);
       totalError++;
-      console.error('     Error');
+      processed = totalError + totalSuccess;     
    } finally {
-      var processed = totalError + totalSuccess;
-      console.clear();
-      console.log(`Processed ${processed}/${totalFiles}`);
-      console.log(`Total Success: ${totalSuccess}`);
-      console.log(`Total Error: ${totalError}`);
+      //spinner.stop();
+      // console.clear();
+      // console.log(`Processed ${processed}/${totalFiles}`);
+      // console.log(`Total Success: ${totalSuccess}`);
+      // console.log(`Total Error: ${totalError}`);
    }
 }
 
-async function trackResult( ws, filePath, response) {
-   ws.write(`${JSON.stringify(flatten(_.get(response,'data')),null,null)},${os.EOL}`);
+async function trackResult(ws, filePath, response) {
+   ws.write(`${JSON.stringify(flatten(_.get(response, 'data')), null, null)},${os.EOL}`);
 }
